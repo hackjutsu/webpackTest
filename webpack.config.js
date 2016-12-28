a7ed8877e7d1c0458823ebc6558784f6f14f7a5d
hackjutsu <wenzhe89929@ucla.edu>
2016-12-28 14:58:30 -0800

    Untabified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-	entry:  './src',
-	output: {
-		path: 'builds',
-		filename: 'bundle.js',
-	},
-	module: {
-		loaders: [
-			{
-				test:/\.js/,
-				loader:  'babel',
-				include: __dirname + '/src',
-			},
-			{
-				test:   /\.scss/,
-				loader: 'style!css!sass',
-				// Or
-				//loaders: ['style', 'css', 'sass'],
-			},
-			{
-				test:   /\.html/,
-				loader: 'html',
-			}
-		],
-	}
+  entry:  './src',
+  output: {
+    path: 'builds',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test:/\.js/,
+        loader:  'babel',
+        include: __dirname + '/src',
+      },
+      {
+        test:   /\.scss/,
+        loader: 'style!css!sass',
+        // Or
+        //loaders: ['style', 'css', 'sass'],
+      },
+      {
+        test:   /\.html/,
+        loader: 'html',
+      }
+    ],
+  }
 };
